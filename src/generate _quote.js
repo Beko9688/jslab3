a378cbc99f84a7981675b49d89e8b5270e27b057
Beko9688 <alexandrov-2021@list.ru>
2022-11-26 01:38:03 +0700

    first cm in the pr

---
+++ src/generate _quote.js
@@ -0,0 +1,5 @@
+function generate_quote(){
+    return "Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine."
+}
+
+export default generate_quote
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import generate_quote from "./generate _quote";
+console.log(generate_quote())
